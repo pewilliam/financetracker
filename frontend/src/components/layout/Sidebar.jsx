9534f95f7063202b650d9c6b5fb1f746252c720d
Pedro William <68715618+pewilliam@users.noreply.github.com>
2026-09-17 10:31:46 -0300

    fix: import from lucide-reacts

--- frontend/src/components/layout/Sidebar.jsx
+++ frontend/src/components/layout/Sidebar.jsx
@@ -1,6 +1,15 @@
 import { useEffect, useRef } from "react";
 import { Link, NavLink } from "react-router-dom";
-import { BarChart3, CalendarDays, ChartNoAxesCombined, ChartPie, CreditCard, ChevronsLeft, ChevronsRight, LogOut, Moon, Settings, Sun, Wallet } from "lucide-react";
+import {
+  LayoutDashboard,
+  CalendarDays,
+  Receipt,
+  Wallet,
+  ChartPie,
+  Layers,
+  Calculator,
+  Coins
+} from "lucide-react";
 import { useAuth } from "../../hooks/useAuth.jsx";
 import { useTheme } from "../../hooks/useTheme.js";
 import { useI18n } from "../../i18n/index.ts";
